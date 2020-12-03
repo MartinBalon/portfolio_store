dbaefd1e384e945b518d984d037d4921204f7ad3
Martin Balon <martinbalon01@gmail.com>
2020-12-03 22:19:44 +0100

    carousel working now - addding circles now

--- src/components/home/carousel/Carousel.js
+++ src/components/home/carousel/Carousel.js
@@ -34,32 +34,49 @@ const slides = [
 let currentSlideId = slides.length;
 
 const changeSlide = (direction) => {
- 
-    if (direction === 'right' && currentSlideId > 1) {
-        const elementA = document.getElementById(currentSlideId);
-        elementA.style.transform = 'translateX(100%)';
-        elementA.style.left = '0%';
-        elementA.style.transition = 'all 1s';
-    
-        currentSlideId--;
-    
-        const elementB = document.getElementById(currentSlideId);
-        elementB.style.left = '-100%';
-        elementB.style.transform = 'translateX(100%)';
-        elementB.style.transition = 'all 1s'; 
-    } else if (direction === 'left' && currentSlideId < 4) {
-        const elementA = document.getElementById(currentSlideId);
-        elementA.style.left = '0%';
-        elementA.style.transform = 'translateX(-100%)';
-        elementA.style.transition = 'all 1s';
-        
-        currentSlideId++;
+    if (direction === 'right') {
+        // move other slides 'behind the scene' so we can slide them from left to right again
+        for(let i = 1; i < slides.length + 1; i++) {
+            if (i !== currentSlideId && i !== (currentSlideId + 1)) {
+                moveSlide(i, 0, -100, 1, 0);
+            }
+        }
+        // slide current slide to the right
+        moveSlide(currentSlideId, 0, 100, 3, 0.5);
+        // restart counter in order to create infinite loop of slides
+        if (currentSlideId === 1) {
+            currentSlideId = slides.length;
+        } else {
+            currentSlideId--; 
+        }
+        // slide next slide to the screen
+        moveSlide(currentSlideId, -100, 100, 3, 0.5);  
+    } else {
+        // move other slides 'behind the scene' so we can slide them from right to left again
+        for(let i = 1; i < slides.length + 1; i++) {
+            if (i !== currentSlideId && i !== (currentSlideId + 1)) {
+                moveSlide(i, 0, 100, 1, 0);
+            }
+        }
+        // slide current slide to the left
+        moveSlide(currentSlideId, 0, -100, 3, 0.5);
+        // restart counter in order to create infinite loop of slides
+        if (currentSlideId === 4) {
+            currentSlideId = 1;
+        } else {
+            currentSlideId++; 
+        }
+        // slide next slide to the screen
+        moveSlide(currentSlideId, 100, -100, 3, 0.5); 
+    }      
+};
 
-        const elementB = document.getElementById(currentSlideId);
-        elementB.style.transform = 'translateX(0%)';
-        elementB.style.transition = 'all 1s'; 
-    }
-      
+const moveSlide = (elementId, leftPercent, translatePercent, zindex, timing) => {
+    const element = document.getElementById(elementId);
+    element.style.zIndex = zindex;
+    element.style.left = `${leftPercent}%`;
+    element.style.transform = `translateX(${translatePercent}%)`;
+    element.style.transition = `all ${timing}s`; 
 };
 
 const Carousel = () => {
@@ -67,7 +84,16 @@ const Carousel = () => {
         <div id="carousel">
             <img src="" id="arrow_left" alt="arrow left logo" onClick={() => changeSlide('left')} />
             <img src="" id="arrow_right" alt="arrow right logo" onClick={() => changeSlide('right')} />
-            <div id="circles"></div>
+            <div id="circles">
+                <div id="circles_container">
+                {slides.map((slide) => 
+                    slide.id === 1 ? 
+                    <div key={slide.id} id={"circle" + slide.id} className="circle circle_active"></div>
+                    : 
+                    <div key={slide.id} id={"circle" + slide.id} className="circle"></div>
+                )} 
+                </div>
+            </div>
             {slides.map(slide =>(
                 <div id={slide.id} key={slide.id} className="slide" style={{  
                     backgroundColor: `${slide.backgroundColor}`
